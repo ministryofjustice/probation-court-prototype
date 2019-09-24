73fcfe2446b58411faa656901f78e5fda78067a8
Paul Massey <paul.massey@scriptwerx.io>
2019-09-24 15:14:54 +0100

    Corrected data URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default {
-  dataUrl: '_self' in React.createElement('div') ? 'http://localhost:8080/api/bigcaselist' : 'https://court-list-mock-data.apps.live-1.cloud-platform.service.justice.gov.uk/api/bigcaselist'
+  dataUrl: 'https://court-list-mock-data.apps.live-1.cloud-platform.service.justice.gov.uk/api/bigcaselist'
 }
