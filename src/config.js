9ea9e00aac2820040c57514d053fbb36eac6290f
Paul Massey <paul.massey@scriptwerx.io>
2019-09-24 15:48:56 +0100

    Made production URL default.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  dataUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/bigcaselist' : 'https://court-list-mock-data.apps.live-1.cloud-platform.service.justice.gov.uk/api/bigcaselist'
+  dataUrl: 'https://court-list-mock-data.apps.live-1.cloud-platform.service.justice.gov.uk/api/bigcaselist'
 }
